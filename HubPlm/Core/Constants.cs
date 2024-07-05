aedabab7849232a399c776dd33d31208ca7abf8f
V4SS3UR <anthony0vasseur@gmail.com>
2024-07-05 19:40:47 +0200

    The file acquisition is now async.

--- HubPlm/Core/Constants.cs
+++ HubPlm/Core/Constants.cs
@@ -1,23 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace ApplicationHub.Properties
 {
     public class Constants
     {
-        private static List<string[]> applicationPathList;
+        public static ObservableCollection<string[]> ApplicationPathList;
+        private static readonly object _lock = new object();
 
-        private static void Init() // Procédure permettant de récupérer les données du fichier DATA
+
+        static Constants()
+        {
+            ApplicationPathList = new ObservableCollection<string[]>();
+            BindingOperations.EnableCollectionSynchronization(ApplicationPathList, _lock);
+            Init();
+        }
+
+        private static async void Init() // Procédure permettant de récupérer les données du fichier DATA
         {
             string AssemblyLocation = AppDomain.CurrentDomain.BaseDirectory;
             string InitFileLocation = Path.Combine(AssemblyLocation, "ApplicationList.txt");
             string Category = string.Empty;
-
-            applicationPathList = new List<string[]>();
-            foreach (string ligne in File.ReadAllLines(InitFileLocation))
+            
+            foreach (string ligne in File.ReadAllLines(InitFileLocation).ToArray())
             {
+                
                 if (ligne.Length == 0) continue;
                 if (ligne.Contains("//")) continue;
 
@@ -29,29 +40,46 @@ namespace ApplicationHub.Properties
 
                 if (ligne.Contains(".exe"))
                 {
-                    applicationPathList.Add(new string[] { ligne, Category });
+                    ApplicationPathList.Add(new string[] { ligne, Category });
                 }
                 else
                 {
-                    // Retrieve executable files
-                    FileInfo[] exeFiles = new DirectoryInfo(ligne).GetFiles("*.exe", SearchOption.TopDirectoryOnly);
-                    if (exeFiles.Length > 0)
-                        applicationPathList.AddRange(exeFiles.Select(o => new string[] { o.FullName, Category }));
-
-                    // Retrieve executable shortcuts
-                    string[] shortcutFiles = Directory.GetFiles(ligne, "*.lnk");
-                    if (shortcutFiles.Length > 0)
-                        applicationPathList.AddRange(shortcutFiles.Where(o => o.Contains(".exe")).Select(o => new string[] { o, Category }));
+                    await Task.Run(() =>
+                    {
+                        GetFiles(ligne, Category);
+                    });
                 }
-            }
+
+            };
         }
 
-        public static string[][] GetPaths()
-        {
-            if (applicationPathList == null)
-                Init();
+        private static async Task GetFiles(string path, string category)
+        {            
+            // Retrieve executable files
+            FileInfo[] exeFiles = new DirectoryInfo(path).GetFiles("*.exe", SearchOption.TopDirectoryOnly);
+            if (exeFiles.Length > 0)
+            {
+                foreach (FileInfo exeFile in exeFiles)
+                {
+                    App.Current.Dispatcher.Invoke(() =>
+                    {
+                        ApplicationPathList.Add(new string[] { exeFile.FullName, category });
+                    });
+                }
+            }
 
-            return applicationPathList.ToArray();
+            // Retrieve executable shortcuts
+            FileInfo[] shortcutFiles = new DirectoryInfo(path).GetFiles("*.lnk", SearchOption.TopDirectoryOnly);
+            if (shortcutFiles.Length > 0)
+            {
+                foreach (FileInfo shortcutFile in shortcutFiles.Where(o => o.Name.Contains(".exe")).ToArray())
+                {
+                    App.Current.Dispatcher.Invoke(() =>
+                    {
+                        ApplicationPathList.Add(new string[] { shortcutFile.FullName, category });
+                    });
+                }
+            }
         }
     }
 }
